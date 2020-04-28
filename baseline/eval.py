7132c787d29f6d4da64c15a3bb0fde1cb5c93571
Ubuntu <ubuntu@ip-172-31-37-153.ec2.internal>
2020-04-28 19:22:32 +0000

    Added big fig

--- baseline/eval.py
+++ baseline/eval.py
@@ -13,10 +13,8 @@ conf_mat = confusion_matrix(data['label_code'].values, data['cluster'].values)
 assignments = np.argmax(conf_mat, axis=0)
 print(assignments)
 
-agg = data.groupby(['label_code']).agg({'cluster':lambda x: np.unique(x, return_counts=True)}).reset_index()
-print(agg)
 
 plt.figure(figsize=(32,32))
 sns.heatmap(conf_mat, annot=True)
-plt.savefig('figures/confusion.png', bbox_inches='tight')
+plt.savefig('figures/confusion.png', bbox_inches='tight', dpi=200)
 plt.close()

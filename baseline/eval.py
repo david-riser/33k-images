876dc29a1281b53d13ec4246468c64ec599feb4d
Ubuntu <ubuntu@ip-172-31-37-153.ec2.internal>
2020-04-28 18:53:09 +0000

    sf

--- baseline/eval.py
+++ baseline/eval.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
@@ -9,8 +10,13 @@ data = pd.read_csv('clusters.csv')
 encoder = LabelEncoder()
 data['label_code'] = encoder.fit_transform(data['label'].values)
 conf_mat = confusion_matrix(data['label_code'].values, data['cluster'].values)
+assignments = np.argmax(conf_mat, axis=0)
+print(assignments)
+
+agg = data.groupby(['label_code']).agg({'cluster':lambda x: np.unique(x, return_counts=True)}).reset_index()
+print(agg)
 
 plt.figure(figsize=(8,6))
-plt.imshow(conf_mat, cmap=plt.cm.Reds)
+sns.heatmap(conf_mat, annot=True)
 plt.savefig('figures/confusion.png', bbox_inches='tight')
 plt.close()

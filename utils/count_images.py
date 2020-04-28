3086d3ac235ec96190fb9e79a238bb6c669bb278
Ubuntu <ubuntu@ip-172-31-29-205.ec2.internal>
2020-04-28 12:50:34 +0000

    Added simple script to count the number of images in top classes and their fractional contribution to the total.

--- utils/count_images.py
+++ utils/count_images.py
@@ -1,10 +1,23 @@
+import numpy as np
 import os
 
+
+data = {}
 folders = 0
 count = 0 
-for folder, _, files in os.walk('../'):
+for folder, _, files in os.walk('../../work_images'):
     folders += 1
     count += len(files)
-
+    data[folder] = len(files)
 
 print(folders, count)
+
+folders = np.array(list(data.keys()))
+images = np.array(list(data.values()))
+indices = np.argsort(images)[::-1]
+running = 0
+
+for i in indices[:80]:
+    running += images[i]
+    print("{0} ({1}): {2}/{3} {4:6.4f}%".format(folders[i], images[i],
+                                      running, count, 100 * float(running/count)))

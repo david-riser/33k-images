ed757e95ec526f195795e7565df373af94ab55c1
Ubuntu <ubuntu@ip-172-31-29-106.ec2.internal>
2020-04-29 13:02:58 +0000

    Added ARS and figures (exp)

--- baseline/eval.py
+++ baseline/eval.py
@@ -1,30 +1,60 @@
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 
+# Some clustering utilities 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import adjusted_rand_score, confusion_matrix
 
+# Compute the Adjusted Rand Score
 data = pd.read_csv('clusters.csv')
 encoder = LabelEncoder()
 data['label_code'] = encoder.fit_transform(data['label'].values)
-conf_mat = confusion_matrix(data['label_code'].values, data['cluster'].values)
-assignments = np.argmax(conf_mat, axis=0)
-print(assignments)
+ar_score = adjusted_rand_score(data['label_code'].values, data['cluster'].values)
+print("Adjusted Rand Score: {0:8.6f}".format(ar_score))
 
+# Print it a few times for random trials
+for i in range(12):
+    ar_score = adjusted_rand_score(data['label_code'].sample(frac=1).values,
+                                   data['cluster'].values)
+    print("Adjusted Rand Score (randomized): {0:8.6f}".format(ar_score))
 
-plt.figure(figsize=(12,12))
-sns.heatmap(conf_mat, annot=True, fmt='d')
-plt.savefig('figures/confusion.png', bbox_inches='tight', dpi=200)
+ar_score = adjusted_rand_score(data['cluster'].values,
+                               data['cluster'].values)
+print("Adjusted Rand Score (exact labels): {0:8.6f}".format(ar_score))
+
+# Number of classes
+n_classes = data['label_code'].nunique()
+side_len = int(np.ceil(np.sqrt(len(data))))
+print(f"Creating {side_len} side length figure for {n_classes} classes.")
+
+colors = cm.rainbow(0, 1, n_classes)
+print(colors)
+
+ideal_figure = np.zeros((side_len, side_len))
+
+for row in range(n_classes):
+    for col in range(n_classes):
+        index = col + row * side_len
+        ideal_figure[row, col] = data['label_code'].values[index]
+
+# Create the map from ideal points
+plt.figure(figsize=(8,6), dpi=100)
+plt.imshow(ideal_figure)
+plt.savefig('figures/ideal_colormap.png', bbox_inches='tight')
 plt.close()
 
-plt.figure(figsize=(12,12))
-sns.heatmap(np.divide(conf_mat, conf_mat.sum(axis=0)), annot=True, fmt='.2f')
-plt.savefig('figures/conflusion_normed_by_truth.png', bbox_inches='tight', dpi=200)
+not_ideal_figure = np.zeros((side_len, side_len))
+
+for row in range(n_classes):
+    for col in range(n_classes):
+        index = col + row * side_len
+        not_ideal_figure[row, col] = data['cluster'].values[index]
+
+# Create the map from ideal points
+plt.figure(figsize=(8,6), dpi=100)
+plt.imshow(ideal_figure)
+plt.savefig('figures/not_ideal_colormap.png', bbox_inches='tight')
 plt.close()
 
-plt.figure(figsize=(12,12))
-sns.heatmap(np.divide(conf_mat, conf_mat.sum(axis=1)), annot=True, fmt='.2f')
-plt.savefig('figures/conflusion_normed_by_cluster_size.png', bbox_inches='tight', dpi=200)
-plt.close()

bae6d2d2a99608a9e4ffc6ade7908fe49ecc6686
Ubuntu <ubuntu@ip-172-31-22-98.ec2.internal>
2020-04-30 13:18:08 +0000

    Upgraded the evaluation code.

--- baseline/eval.py
+++ baseline/eval.py
@@ -1,60 +1,64 @@
+import glob
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
 
 # Some clustering utilities 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import adjusted_rand_score, confusion_matrix
 
-# Compute the Adjusted Rand Score
-data = pd.read_csv('clusters.csv')
-encoder = LabelEncoder()
-data['label_code'] = encoder.fit_transform(data['label'].values)
-ar_score = adjusted_rand_score(data['label_code'].values, data['cluster'].values)
-print("Adjusted Rand Score: {0:8.6f}".format(ar_score))
 
-# Print it a few times for random trials
-for i in range(12):
-    ar_score = adjusted_rand_score(data['label_code'].sample(frac=1).values,
-                                   data['cluster'].values)
-    print("Adjusted Rand Score (randomized): {0:8.6f}".format(ar_score))
+def plot_cluster_viz(true_label_sorted_data, pred_label_sorted_data,
+                     backbone, pooling):
+    """ 
+    Create a figure for the visualization of clustering results. 
+    """
+    side_len = int(np.ceil(np.sqrt(len(data))))
+    n_classes = true_label_sorted_data['label_code'].nunique()
 
-ar_score = adjusted_rand_score(data['cluster'].values,
-                               data['cluster'].values)
-print("Adjusted Rand Score (exact labels): {0:8.6f}".format(ar_score))
+    # Create images
+    ideal_figure = np.zeros((side_len, side_len))
+    not_ideal_figure = np.zeros((side_len, side_len))
+    for row in range(side_len):
+        for col in range(side_len):
+            index = col + row * side_len
+            if index < len(data):
+                ideal_figure[row, col] = true_label_sorted_data['label_code'].values[index]
+                not_ideal_figure[row, col] = pred_label_sorted_data['label_code'].values[index]
 
-# Number of classes
-n_classes = data['label_code'].nunique()
-side_len = int(np.ceil(np.sqrt(len(data))))
-print(f"Creating {side_len} side length figure for {n_classes} classes.")
+    # Create the map from ideal points
+    for cmap in ['rainbow']:
+        plt.figure(figsize=(16,6), dpi=100)
+        plt.subplot(1, 2, 1)
+        plt.imshow(ideal_figure, cmap=plt.cm.get_cmap(cmap, n_classes))
+        plt.title('True Label Assignment')
+        plt.subplot(1, 2, 2)
+        plt.imshow(not_ideal_figure, cmap=plt.cm.get_cmap(cmap, n_classes))
+        plt.title('Cluster Assignment')
+        plt.savefig('figures/cluster_{}_{}_colormap_{}.png'.format(backbone, pooling, cmap.lower()),
+                    bbox_inches='tight')
+        plt.close()
 
-colors = cm.rainbow(0, 1, n_classes)
-print(colors)
+if __name__ == "__main__":
 
-# For figure
-true_label_sorted_data = data.sort_values('label_code')
-pred_label_sorted_data = data.sort_values('cluster')
+    input_folder = './artifacts'
+    data = {}
+    rand_index = {}
+    for csvfile in glob.glob(input_folder + '/*'):
 
-ideal_figure = np.zeros((side_len, side_len))
-not_ideal_figure = np.zeros((side_len, side_len))
-for row in range(side_len):
-    for col in range(side_len):
-        index = col + row * side_len
-        if index < len(data):
-            ideal_figure[row, col] = true_label_sorted_data['label_code'].values[index]
-            not_ideal_figure[row, col] = pred_label_sorted_data['label_code'].values[index]
+        backbone = csvfile.split('/')[-1].split('_')[0]
+        pooling = csvfile.split('/')[-1].split('_')[1]
 
-# Create the map from ideal points
-for cmap in ['rainbow', 'plasma', 'viridis', 'Reds', 'Blues']:
-    plt.figure(figsize=(16,6), dpi=100)
-    plt.subplot(1, 2, 1)
-    plt.imshow(ideal_figure, cmap=plt.cm.get_cmap(cmap, n_classes))
-    plt.title('True Label Assignment')
-    plt.subplot(1, 2, 2)
-    plt.imshow(not_ideal_figure, cmap=plt.cm.get_cmap(cmap, n_classes))
-    plt.title('Cluster Assignment')
-    plt.savefig('figures/cluster_colormap_{}.png'.format(cmap.lower()),
-                bbox_inches='tight')
-    plt.close()
+        data[csvfile] = pd.read_csv(csvfile)
+        encoder = LabelEncoder()
+        data[csvfile]['label_code'] = encoder.fit_transform(data[csvfile]['label'].values)
+        rand_index[csvfile] = adjusted_rand_score(
+            data[csvfile]['label_code'].values, data[csvfile]['cluster'].values)
+        print("Adjusted Rand Score ({0}): {1:8.6f}".format(csvfile, rand_index[csvfile]))
+
+        true_label_sorted_data = data[csvfile].sort_values('label_code')
+        pred_label_sorted_data = data[csvfile].sort_values('cluster')
+        plot_cluster_viz(true_label_sorted_data, pred_label_sorted_data, backbone, pooling)
 

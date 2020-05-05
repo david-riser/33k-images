788fcc157b0372fe477ee8ac00a02240c40f9ef0
Ubuntu <ubuntu@ip-172-31-30-253.ec2.internal>
2020-05-05 15:31:10 +0000

    added simple figure

--- utils/count_images.py
+++ utils/count_images.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -18,7 +19,17 @@ images = np.array(list(data.values()))
 indices = np.argsort(images)[::-1]
 running = 0
 
+total = []
 for i in indices:
     running += images[i]
     print("{0} ({1}): {2}/{3} {4:6.4f}%".format(folders[i], images[i],
                                                 running, count, 100 * float(running/count)))
+    total.append(100 * float(running / count))
+
+
+plt.figure(figsize=(12,5))
+plt.plot(np.arange(1,len(total)+1), total)
+plt.grid(alpha=0.2)
+plt.xlabel('Classes Included (Largest to Smallest)')
+plt.ylabel('Percent of Dataset')
+plt.savefig('percent_of_dataset.png', bbox_inches='tight')

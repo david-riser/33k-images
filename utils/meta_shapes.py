745edc2bb38d35ac6dc0ec5312267d374963de0b
Ubuntu <ubuntu@ip-172-31-25-8.ec2.internal>
2020-05-06 12:19:00 +0000

    Solved greyscale bug

--- utils/meta_shapes.py
+++ utils/meta_shapes.py
@@ -3,7 +3,9 @@ import os
 import numpy as np
 import pandas as pd
 
-from PIL import Image
+from PIL import Image, ImageChops
+from tensorflow.keras.preprocessing.image import load_img
+
 
 if __name__ == "__main__":
     
@@ -15,6 +17,7 @@ if __name__ == "__main__":
     data = {}
     data['image'] = []
     data['shape'] = []
+    data['greyscale'] = []
     for folder, b, files in os.walk(args.data):
         for image in files:
             if image.startswith('./'):
@@ -26,13 +29,10 @@ if __name__ == "__main__":
             
             try:
                 if image_path.count('.') < 3 and ext not in ['zip', '7z', 'py', 'py~', 'db', 'txt']:
-                    numpy_image = np.asarray(Image.open(image_path))
-
-                    if len(numpy_image.shape) == 2:
-                        numpy_image = numpy_image.reshape(*numpy_image.shape, 1)
-
+                    numpy_image = np.asarray(load_img(image_path))
                     data['image'].append(image_path)
                     data['shape'].append(numpy_image.shape)
+                    data['greyscale'].append(numpy_image.std(axis=2).mean() == 0)
                     
             except Exception as e:
                 print("Exception {}".format(e))

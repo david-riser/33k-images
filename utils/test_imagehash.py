4b8c5dfdc2579d95d331509d27fe611ad1dc6d67
Ubuntu <ubuntu@ip-172-31-20-234.ec2.internal>
2020-05-11 19:59:17 +0000

    Added hashes study

--- utils/test_imagehash.py
+++ utils/test_imagehash.py
@@ -1,90 +1,182 @@
 import numpy as np
 import imagehash
+import json
 import time
 
 from PIL import Image
 
-def hash_duplicate_search(images1, images2):
-    """ 
-
-    Search the two image sets for common images.  This is 
-    done by hashing the two images and using a sorted hashtable.
-
-    """
-    hashtable = {}
-    matches = []
-    for i, image in enumerate(images2):
-        hashtable[imagehash.dhash(Image.fromarray(image))] = i
-
-    for i, image in enumerate(images1):
-        image_hash = imagehash.dhash(Image.fromarray(image))
-        if image_hash in hashtable:
-            matches.append((i, hashtable[image_hash]))
-
-    return matches
-
-def diff_duplicate_search(images1, images2):
+def hash_duplicate_search(images):
     """ 
     
-    Take a difference and see if two images are the same.
+    Search one set of images for duplicates.  The
+    method used here is hashing the image and 
+    then mapping it into the hashmap.  This finds 
+    exact matches but also reports collisions in the
+    hashtable.  Therefore the duplicate matching 
+    algorithm must be used on the output of this.
 
+    """
+
+    hashtable = {}
+    matches = []
+    for i, image in enumerate(images):
+        hashed = imagehash.dhash(Image.fromarray(image))
+        if hashed not in hashtable:
+            hashtable[hashed] = i
+        else:
+            matches.append((hashtable[hashed],i))
+        
+    return matches
+
+def diff_duplicate_search(images):
+    """ 
+
+    Search one set of images for duplicates using 
+    the different method. 
+    
     """
     matches = []
     def check(img1, img2):
         arr1, arr2 = np.asarray(img1), np.asarray(img2)
         return np.sum((arr2 - arr1)**2) == 0
 
-    for i, image1 in enumerate(images1):
-        for j, image2 in enumerate(images2):
-            if check(image1, image2):
+    for i in range(len(images)):
+        for j in range(i+1,len(images)):
+            if check(images[i], images[j]):
                 matches.append((i,j))
+    
 
     return matches
 
 
-def create_image_dataset(imagesize, samples1, samples2, overlaps):
+def create_image_dataset(imagesize, samples, overlaps):
     """ Create a testing set. """
 
-    images1 = []
-    images2 = []
+    images = []
     matches = []
     
-    assert(overlaps < samples1 and overlaps < samples2)
-    for _ in range(samples1 - overlaps):
-        images1.append(np.random.randint(
-            0, 255, size=imagesize, dtype=np.uint8
-        ))
-    for _ in range(samples2 - overlaps):
-        images2.append(np.random.randint(
-            0, 255, size=imagesize, dtype=np.uint8
-        ))
-    for _ in range(overlaps):
-        image = np.random.randint(
-            0, 255, size=imagesize, dtype=np.uint8
-        )
-        images1.append(image)
-        images2.append(image)
-        matches.append((len(images1) - 1,len(images2) - 1))
-
-    return images1, images2, matches
+    if overlaps >= samples:
+        print('[FATAL] The number of overlaps has to be smaller than the number of samples.')
+        exit()
         
+    for _ in range(samples - overlaps):
+        images.append(np.random.randint(
+            0, 255, size=imagesize, dtype=np.uint8
+        ))
+
+    duplicates = np.random.choice(np.arange(0, len(images)-1),
+                                 overlaps, replace=False)
+    for dupe in duplicates:
+        images.append(np.copy(images[dupe]))
+        matches.append((dupe,len(images)-1))
+        
+    return images, matches
+
+def validate(y_true, y_pred):
+    total = len(y_true)
+    matches = 0
+
+    for yp in y_pred:
+        if yp in y_true:
+            matches += 1
+
+    return float(matches / total)
+
 if __name__ == "__main__":
 
     image_shape = (36, 36, 3)
 
     # Generate a testing set.
-    images1, images2, matches = create_image_dataset(
+    images, matches = create_image_dataset(
         imagesize=image_shape,
-        samples1=2000,
-        samples2=4000,
-        overlaps=12
+        samples=100,
+        overlaps=10
     )
-    print("True Matches: ", matches)
 
-    timer_start = time.time()
-    print("Hash Matches: ", hash_duplicate_search(images1, images2))
-    print("Elapsed Time: {}".format(time.time() - timer_start))
+    studies = {}
+    studies['sample_size'] = []
+    studies['overlap_size'] = []
+    studies['image_size'] = []
 
-    timer_start = time.time()
-    print("Diff Matches: ", diff_duplicate_search(images1, images2))
-    print("Elapsed Time: {}".format(time.time() - timer_start))
+    # Study the performance w.r.t. sample size
+    sample_sizes = [10, 100, 1000]
+    for sample_size in sample_sizes:
+        data = {}
+        data['sample_size'] = sample_size
+        
+        images, matches = create_image_dataset(
+            imagesize=image_shape,
+            samples=sample_size,
+            overlaps=int(0.1 * sample_size)
+        )
+        
+        # Test difference method.
+        start_time = time.time()
+        dmatches = diff_duplicate_search(images)
+        data['diff_search_time'] = time.time() - start_time
+        data['diff_search_validity'] = validate(matches, dmatches)
+
+        # Test hash method. .
+        start_time = time.time()
+        dmatches = hash_duplicate_search(images)
+        data['hash_search_time'] = time.time() - start_time
+        data['hash_search_validity'] = validate(matches, dmatches)
+
+        print(data)
+        studies['sample_size'].append(data)
+        
+    overlap_sizes = [0.01, 0.05, 0.1, 0.2, 0.4]
+    for overlap in overlap_sizes:
+        data = {}
+        data['overlap'] = overlap
+        
+        images, matches = create_image_dataset(
+            imagesize=image_shape,
+            samples=1000,
+            overlaps=int(overlap * 1000)
+        )
+        
+        # Test difference method.
+        start_time = time.time()
+        dmatches = diff_duplicate_search(images)
+        data['diff_search_time'] = time.time() - start_time
+        data['diff_search_validity'] = validate(matches, dmatches)
+
+        # Test hash method. .
+        start_time = time.time()
+        dmatches = hash_duplicate_search(images)
+        data['hash_search_time'] = time.time() - start_time
+        data['hash_search_validity'] = validate(matches, dmatches)
+
+        print(data)
+        studies['overlap_size'].append(data)
+
+    image_sizes = [(8,8,3), (16,16,3), (32,32,3), (64,64,3), (128,128,3)]
+    for image_size in image_sizes:
+        data = {}
+        data['image_size'] = image_size
+        
+        images, matches = create_image_dataset(
+            imagesize=image_size,
+            samples=1000,
+            overlaps=100
+        )
+        
+        # Test difference method.
+        start_time = time.time()
+        dmatches = diff_duplicate_search(images)
+        data['diff_search_time'] = time.time() - start_time
+        data['diff_search_validity'] = validate(matches, dmatches)
+
+        # Test hash method. .
+        start_time = time.time()
+        dmatches = hash_duplicate_search(images)
+        data['hash_search_time'] = time.time() - start_time
+        data['hash_search_validity'] = validate(matches, dmatches)
+
+        print(data)
+        studies['image_size'].append(data)
+
+    
+    with open('hashes_study.json', 'w') as out:
+        json.dump(studies, out, indent=4)

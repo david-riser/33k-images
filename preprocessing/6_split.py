41b0f43761c5cb442cee6d7f4c8d5a1d03460726
Ubuntu <ubuntu@ip-172-31-24-222.ec2.internal>
2020-05-18 15:38:42 +0000

    Added some change to pre-processing including a script to generate train/dev/test folds

--- preprocessing/6_split.py
+++ preprocessing/6_split.py
@@ -1,3 +1,10 @@
+""" 
+
+This script is optional, it will split the 
+data into train/dev/test.
+
+"""
+
 import argparse
 import numpy as np
 import os
@@ -6,7 +13,7 @@ import os
 def get_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('--data', required=True, type=str)
-    ap.add_argument('--output', required=True, type=str)
+    ap.add_argument('--output_dir', required=True, type=str)
     ap.add_argument('--dev_frac', type=float, default=0.1)
     ap.add_argument('--test_frac', type=float, default=0.1)
     return ap.parse_args()
@@ -24,17 +31,23 @@ if __name__ == "__main__":
     # dev/test.
     threshold = args.dev_frac + args.test_frac
     
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
-        os.makedirs(os.path.join(args.output,'train'))
-        os.makedirs(os.path.join(args.output,'dev'))
-        os.makedirs(os.path.join(args.output,'test'))
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        os.makedirs(os.path.join(args.output_dir,'train'))
+        os.makedirs(os.path.join(args.output_dir,'dev'))
+        os.makedirs(os.path.join(args.output_dir,'test'))
         
     for folder, _, files in os.walk(args.data):
+
+        # Please do not process the
+        # input folder as a class.
+        if folder == args.data:
+            continue
+        
         root_folder = folder.split('/')[-1]
-        os.makedirs(os.path.join(args.output,'train/' + root_folder))
-        os.makedirs(os.path.join(args.output,'dev/' + root_folder))
-        os.makedirs(os.path.join(args.output,'test/' + root_folder))
+        os.makedirs(os.path.join(args.output_dir,'train/' + root_folder))
+        os.makedirs(os.path.join(args.output_dir,'dev/' + root_folder))
+        os.makedirs(os.path.join(args.output_dir,'test/' + root_folder))
         
         if root_folder not in prohibited_folders:
             print('Working on {}.'.format(root_folder))
@@ -46,7 +59,7 @@ if __name__ == "__main__":
                     if (np.random.uniform() > threshold):
                         os.system('cp {} {}'.format(
                             os.path.join(folder, f),
-                            os.path.join(args.output + '/train/' + root_folder, f)
+                            os.path.join(args.output_dir + '/train/' + root_folder, f)
                         ))
                     else:
                         test_threshold = args.test_frac / (args.dev_frac + args.test_frac)
@@ -54,12 +67,12 @@ if __name__ == "__main__":
                         if (np.random.uniform() > test_threshold):
                             os.system('cp {} {}'.format(
                                 os.path.join(folder, f),
-                                os.path.join(args.output + '/dev/' + root_folder, f)
+                                os.path.join(args.output_dir + '/dev/' + root_folder, f)
                             ))
                         else:
                             os.system('cp {} {}'.format(
                                 os.path.join(folder, f),
-                                os.path.join(args.output + '/test/' + root_folder, f)
+                                os.path.join(args.output_dir + '/test/' + root_folder, f)
                             ))
 
     
